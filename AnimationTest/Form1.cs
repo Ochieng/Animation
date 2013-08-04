af3052e371f735c0aa6d9bd8d73739bf692b3736
exoticknight <draco.knight0@gmail.com>
2013-08-04 23:07:47 +0800

    init

---
+++ AnimationTest/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using Animation;
+
+namespace Animation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ControlAnimation.HorizontalMove(label1, 300, 500, AnimationType.Resilience);
+        }
+    }
+}
